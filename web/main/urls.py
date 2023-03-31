0ba935883c20532a6dfad2f0d134b28744c5be84
Milan <sosko666@gmail.com>
2023-03-31 08:50:01 +0200

    prvni krucky

--- web/main/urls.py
+++ web/main/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from main.views import main_html, AboutView
+from main.views import main_html, AboutView, UploadFileView
 
 urlpatterns = [
-    path("", AboutView.as_view()),
+    path("", AboutView.as_view(), name='home'),
+    path("upload_file", UploadFileView.as_view(), name='upload_file'),
 ]
--- web/main/views.py
+++ web/main/views.py
@@ -1,7 +1,10 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.template.loader import get_template
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, FormView
+
+from main.forms import UploadFileForm
+from web.settings import BASE_DIR
 
 
 # Create your views here.
@@ -9,5 +12,27 @@ from django.views.generic import TemplateView
 def main_html(request):
     return get_template('template.html')
 
+
 class AboutView(TemplateView):
-    template_name = "template.html"
+    template_name = "main.html"
+
+
+def handle_uploaded_file(f):
+    with open(BASE_DIR / 'writable' / f.name(), 'wb+') as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
+
+
+class UploadFileView(FormView):
+    form_class = UploadFileForm
+    template_name = 'upload.html'  # Replace with your template.
+
+    def post(self, request, *args, **kwargs):
+        form_class = self.get_form_class()
+        form = self.get_form(form_class)
+        files = request.FILES.getlist('file_field')
+        if form.is_valid():
+            handle_uploaded_file(request.FILES['file'])
+            return HttpResponseRedirect('/upload_list')
+        else:
+            return self.form_invalid(form)
